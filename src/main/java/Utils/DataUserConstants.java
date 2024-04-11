34d3bf0c05b35b17ed79fbd1117921bee51d1a68
Akim Sorokin <molod@PC-Mornil.ru>
2024-04-11 14:17:16 +0300

    added LoginPageTest

--- src/main/java/Utils/DataUserConstants.java
+++ src/main/java/Utils/DataUserConstants.java
@@ -2,7 +2,7 @@ package Utils;
 
 public class DataUserConstants {
 
-    public static final String USER_EMAIL = "user1@mail.ru";
+    public static final String USER_EMAIL = "user12@mail.rs";
     public static final String USER_NAME = "";
     public static final String USER_PASSWORD = "user1";
 }
--- src/main/test/PageTest/SignUpPageTest.java
+++ src/main/test/PageTest/SignUpPageTest.java
@@ -1,17 +1,27 @@
 package PageTest;
 
 import Page.HomePage;
+import Page.SignUpPage;
 import org.openqa.selenium.By;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import static Page.HomePage.SIGN_UP_BUTTON_LOCATOR;
+import static Page.LoginPage.LOGIN_STRING_LOCATOR;
 import static Page.ProfilePage.WELCOME_STRING_LOCATOR;
-import static Page.SignUpPage.*;
+import static Page.SignUpPage.INPUT_EMAIL_FIELD_LOCATOR;
+import static Page.SignUpPage.INPUT_NAME_FIELD_LOCATOR;
+import static Page.SignUpPage.INPUT_PASSWORD_FIELD_LOCATOR;
+import static Page.SignUpPage.SIGN_UP_STRING_LOCATOR;
 import static Utils.DataUserConstants.USER_EMAIL;
 import static Utils.DataUserConstants.USER_PASSWORD;
-import static Utils.DefaultUsingMethods.*;
+import static Utils.DefaultUsingMethods.browserQuit;
+import static Utils.DefaultUsingMethods.clickInteractiveElement;
+import static Utils.DefaultUsingMethods.isDisplay;
+import static Utils.DefaultUsingMethods.openSite;
+import static Utils.DefaultUsingMethods.sendKeys;
 import static org.testng.Assert.assertTrue;
 
 public class SignUpPageTest {
@@ -25,6 +35,7 @@ public class SignUpPageTest {
                 SIGN_UP_STRING_LOCATOR
         };
     }
+
     @DataProvider(name = "userDataTest")
     public static Object[][] dataTestUser() {
         return new Object[][]{
@@ -39,16 +50,17 @@ public class SignUpPageTest {
 
     @Test(dataProvider = "SignUpLocator")
     public static void checkSignUpElementsIsDisplay(By xpath) {
-        clickInteractiveElement(HomePage.SIGN_UP_BUTTON_LOCATOR, SIGN_UP_STRING_LOCATOR);
+        clickInteractiveElement(SIGN_UP_BUTTON_LOCATOR, SIGN_UP_STRING_LOCATOR);
         assertTrue(isDisplay(xpath));
     }
 
     @Test(dataProvider = "userDataTest")
     public static void signUp(String userEmail, String userPassword){
-        clickInteractiveElement(HomePage.SIGN_UP_BUTTON_LOCATOR, SIGN_UP_STRING_LOCATOR);
+        clickInteractiveElement(SIGN_UP_BUTTON_LOCATOR, SIGN_UP_STRING_LOCATOR);
         sendKeys(INPUT_EMAIL_FIELD_LOCATOR, userEmail);
         sendKeys(INPUT_PASSWORD_FIELD_LOCATOR, userPassword);
-        clickInteractiveElement(SIGN_UP_BUTTON_LOCATOR, WELCOME_STRING_LOCATOR);
+        clickInteractiveElement(SignUpPage.SIGN_UP_BUTTON_LOCATOR, LOGIN_STRING_LOCATOR);
+        assertTrue(isDisplay(LOGIN_STRING_LOCATOR));
     }
 
     @AfterTest
